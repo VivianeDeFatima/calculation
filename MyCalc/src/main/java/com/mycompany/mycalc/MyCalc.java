83b3a925de47367f7f0483c4976b9a5e0b00b9d5
Viviane de Fatima <vivianedefatima19@gmail.com>
2024-11-12 15:17:51 -0300

    Inicio de projeto

---
+++ MyCalc/src/main/java/com/mycompany/mycalc/MyCalc.java
@@ -0,0 +1,16 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ */
+
+package com.mycompany.mycalc;
+
+/**
+ *
+ * @author 16812749656
+ */
+public class MyCalc {
+
+    public static void main(String[] args) {
+        System.out.println("Hello World!");
+    }
+}
